74b224773e052218641b0b45210489a3898b41aa
Gabriel Weich <gabrielweich@hotmail.com>
2018-09-02 00:48:57 -0300

    ajustes na statusbar

--- youp/app/containers/DetalheContainer.js
+++ youp/app/containers/DetalheContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, ImageBackground } from 'react-native'
+import { View, Text, ImageBackground, TouchableOpacity, StatusBar } from 'react-native'
 
 
 export default class DetalheContainer extends React.Component {
@@ -8,18 +8,39 @@ export default class DetalheContainer extends React.Component {
         const { name, date, description, uri, category } = this.props.navigation.state.params.projeto
         // console.warn(uri)
         return (
-            <View>
+            <View style={{ flex: 1, backgroundColor: '#fff' }}>
+                <StatusBar
+                    barStyle="dark-content"
+                    backgroundColor="#fff"
+                />
                 <ImageBackground
                     style={{ width: '100%', height: 200 }}
                     source={{ uri }}
                 />
-                <View style={{padding: 20}}>
-                    <Text style={{fontWeight: 'bold'}}>{category.toUpperCase()}</Text>
-                    <Text style={{fontSize: 30}}>{name}</Text>
+                <View style={{ padding: 20, flex: 1 }}>
+                    <Text style={{ fontWeight: 'bold' }}>{category.toUpperCase()}</Text>
+                    <Text style={{ fontSize: 30 }}>{name}</Text>
 
-                    <Text>{description}</Text>
+                    <View style={{ marginTop: 30 }}>
+                        <Text>{description}</Text>
+                    </View>
+                    <View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center' }}>
+                        <TouchableOpacity style={{
+                            width: 250,
+                            marginBottom: 10,
+                            alignItems: 'center',
+                            borderRadius: 10,
+                            backgroundColor: '#68CFF1',
+                            padding: 10
+                        }}>
+                            <Text style={{ color: '#fff' }}>
+                                Quero Participar!
+
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
-                
+
 
             </View>
         )
--- youp/app/containers/HomeContainer.js
+++ youp/app/containers/HomeContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, FlatList, View, Image } from 'react-native'
+import { Text, FlatList, View, Image, StatusBar } from 'react-native'
 import ProjetoItem from '../components/ProjetoItem'
 import CategoriaItem from '../components/CategoriaItem'
 
@@ -25,21 +25,25 @@ export default class HomeContainer extends React.Component {
         ],
         categorias: [
             { id_categoria: '1', name: 'Animais', icon: 'ios-paw', color: 'blue' },
-            { id_categoria: '2', name: 'Saúde', icon: 'ios-medical', color: 'blue'},
-            { id_categoria: '3', name: 'Educação', icon: 'ios-book', color: 'blue'},
-            { id_categoria: '4', name: 'Saúde', icon: 'ios-medical', color: 'blue'}
+            { id_categoria: '2', name: 'Saúde', icon: 'ios-medical', color: 'blue' },
+            { id_categoria: '3', name: 'Educação', icon: 'ios-book', color: 'blue' },
+            { id_categoria: '4', name: 'Saúde', icon: 'ios-medical', color: 'blue' }
         ],
         selected: '2'
     }
 
     selectCategoriaHandler = (idCategoria) => {
         // console.warn(idCategoria)
-        this.setState({selected: idCategoria})
+        this.setState({ selected: idCategoria })
     }
     //7504AE
     render() {
         return (
             <View style={{ backgroundColor: '#fff', flex: 1 }}>
+                <StatusBar
+                    barStyle="dark-content"
+                    backgroundColor="#fff"
+                />
                 <Image style={{ alignSelf: 'center', width: 100, height: 60, justifyContent: 'center' }} resizeMode="contain" source={require('../images/YoUP.gif')} />
                 <View
                     style={{
@@ -52,15 +56,15 @@ export default class HomeContainer extends React.Component {
                 <FlatList
                     extraData={this.state}
                     data={this.state.categorias}
-                    renderItem={({ item }) => <CategoriaItem {...item} selected={this.state.selected == item.id_categoria} select={this.selectCategoriaHandler}/>}
+                    renderItem={({ item }) => <CategoriaItem {...item} selected={this.state.selected == item.id_categoria} select={this.selectCategoriaHandler} />}
                     keyExtractor={(item, index) => index}
                     horizontal
                     showsHorizontalScrollIndicator={false}
                 />
-                <View style={{marginVertical: 5}}/>
+                <View style={{ marginVertical: 5 }} />
                 <FlatList
                     data={this.state.projetos}
-                    renderItem={({ item }) => <ProjetoItem {...item} detalhe={() => this.props.navigation.navigate('DetalheScreen', {projeto: item})}/>}
+                    renderItem={({ item }) => <ProjetoItem {...item} detalhe={() => this.props.navigation.navigate('DetalheScreen', { projeto: item })} />}
                     keyExtractor={item => item.id}
                 />
             </View>
